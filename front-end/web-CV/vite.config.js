063d95e565196e26ae9784b8ef9b50258baec67b
Your Name <your.email@example.com>
2025-05-29 10:28:10 +0700

    update

--- front-end/web-CV/vite.config.js
+++ front-end/web-CV/vite.config.js
@@ -7,7 +7,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/web-cv/', // Thêm base URL cho GitHub Pages
   plugins: [
     vue(),
     vueJsx(),
