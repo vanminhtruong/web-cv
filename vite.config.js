faf91af6f10195170ca1354fb36d313a864b1fe8
Your Name <your.email@example.com>
2025-05-29 12:01:11 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,6 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
-  base: '/web-cv/', // Thêm base path cho GitHub Pages
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
